a165770670dda5b637559a99df5d666556ecb7a6
ahmed6893 <ahmedrifat6893@gmail.com>
2025-05-26 19:37:09 +0600

    adding Vuejs

---
+++ app/Http/Controllers/APIController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use function Illuminate\Foundation\Configuration\respond;
+
+class APIController extends Controller
+{
+    public function getAllCategory()
+    {
+        $categories = Category::with('subCategory')->get();
+        return response()->json($categories);
+    }
+
+    public function getAllTrendingProduct()
+    {
+        $products = Product::where('status',1)->latest()->get(['id','name','image','selling_price']);
+        foreach ($products as $product)
+        {
+            $product->image = asset($product->image);
+        }
+        return response()->json($products);
+    }
+    public function getAllCategoryProduct($id)
+    {
+        $products = Product::where('category_id',$id)->where('status',1)->latest()->get(['id','name','image','selling_price']);
+        foreach ($products as $product)
+        {
+            $product->image = asset($product->image);
+        }
+        return response()->json($products);
+    }
+    public function getProductDetail($id)
+    {
+        $product = Product::find($id);
+        $product->image = asset($product->image);
+        foreach ($product->productImages as $productImage)
+        {
+            $productImage->image = asset($productImage->image);
+        }
+        return response()->json($product);
+    }
+
+}
+
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\APIController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/get-all-category',[APIController::class,'getAllCategory']);
+Route::get('/get-all-trendingProducts',[APIController::class,'getAllTrendingProduct']);
+Route::get('/get-all-CategoryProducts/{id}',[APIController::class,'getAllCategoryProduct']);
+Route::get('/get-product-details/{id}',[APIController::class,'getProductDetail']);
